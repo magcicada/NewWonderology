f752f0ef5132260c8002e07455e3f77d965a3bb6
Me <135455255+IcarussOne@users.noreply.github.com>
2024-08-15 11:31:03 -0500

    Add Cleansing Charm equip sounds

--- src/main/java/com/verdantartifice/thaumicwonders/common/items/baubles/ItemCleansingCharm.java
+++ src/main/java/com/verdantartifice/thaumicwonders/common/items/baubles/ItemCleansingCharm.java
@@ -11,6 +11,7 @@ import net.minecraft.client.resources.I18n;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.item.EnumRarity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagInt;
@@ -125,5 +126,15 @@ public class ItemCleansingCharm extends ItemTW implements IBauble, IRechargable 
     @Override
     public EnumRarity getRarity(ItemStack stack) {
         return EnumRarity.RARE;
+    }
+
+    @Override
+    public void onEquipped(ItemStack stack, EntityLivingBase player) {
+        player.playSound(SoundEvents.ENTITY_ENDEREYE_DEATH, 0.75F, 2.0F);
+    }
+
+    @Override
+    public void onUnequipped(ItemStack stack, EntityLivingBase player) {
+        player.playSound(SoundEvents.ENTITY_ENDEREYE_DEATH, 0.75F, 2.0F);
     }
 }
