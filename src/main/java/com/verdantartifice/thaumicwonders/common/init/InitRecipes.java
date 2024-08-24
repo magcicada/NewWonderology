3aa27fe7f1018557f774512e956f61c83eccd19c
Me <135455255+IcarussOne@users.noreply.github.com>
2024-08-24 00:11:43 -0500

    Merge both hedge alchemy researches together

--- src/main/java/com/verdantartifice/thaumicwonders/common/init/InitRecipes.java
+++ src/main/java/com/verdantartifice/thaumicwonders/common/init/InitRecipes.java
@@ -325,37 +325,37 @@ public class InitRecipes {
 
     private static void initCrucibleRecipes() {
         ThaumcraftApi.addCrucibleRecipe(new ResourceLocation(ThaumicWonders.MODID, "hedge_soul_sand"), new CrucibleRecipe(
-                "TWOND_NETHER_HEDGE",
+                "TWOND_DIMENSIONAL_HEDGE",
                 new ItemStack(Blocks.SOUL_SAND),
                 new ItemStack(Blocks.SAND),
                 new AspectList().add(Aspect.SOUL, 3).add(Aspect.EARTH, 3).add(Aspect.TRAP, 1)
         ));
         ThaumcraftApi.addCrucibleRecipe(new ResourceLocation(ThaumicWonders.MODID, "hedge_ghast_tear"), new CrucibleRecipe(
-                "TWOND_NETHER_HEDGE",
+                "TWOND_DIMENSIONAL_HEDGE",
                 new ItemStack(Items.GHAST_TEAR, 2),
                 new ItemStack(Items.GHAST_TEAR),
                 new AspectList().add(Aspect.SOUL, 10).add(Aspect.ALCHEMY, 10).add(Aspect.UNDEAD, 5)
         ));
         ThaumcraftApi.addCrucibleRecipe(new ResourceLocation(ThaumicWonders.MODID, "hedge_wither_skull"), new CrucibleRecipe(
-                "TWOND_NETHER_HEDGE",
+                "TWOND_DIMENSIONAL_HEDGE",
                 new ItemStack(Items.SKULL, 1, 1),
                 new ItemStack(Items.SKULL, 1, 0),
                 new AspectList().add(Aspect.UNDEAD, 10).add(Aspect.DEATH, 10).add(Aspect.SOUL, 10).add(Aspect.DARKNESS, 5)
         ));
         ThaumcraftApi.addCrucibleRecipe(new ResourceLocation(ThaumicWonders.MODID, "hedge_end_stone"), new CrucibleRecipe(
-                "TWOND_END_HEDGE",
+                "TWOND_DIMENSIONAL_HEDGE",
                 new ItemStack(Blocks.END_STONE),
                 new ItemStack(Blocks.STONE),
                 new AspectList().add(Aspect.EARTH, 5).add(Aspect.DARKNESS, 5)
         ));
         ThaumcraftApi.addCrucibleRecipe(new ResourceLocation(ThaumicWonders.MODID, "hedge_chorus_fruit"), new CrucibleRecipe(
-                "TWOND_END_HEDGE",
+                "TWOND_DIMENSIONAL_HEDGE",
                 new ItemStack(Items.CHORUS_FRUIT),
                 new ItemStack(Items.APPLE),
                 new AspectList().add(Aspect.ELDRITCH, 5).add(Aspect.SENSES, 5).add(Aspect.PLANT, 5)
         ));
         ThaumcraftApi.addCrucibleRecipe(new ResourceLocation(ThaumicWonders.MODID, "hedge_ender_pearl"), new CrucibleRecipe(
-                "TWOND_END_HEDGE",
+                "TWOND_DIMENSIONAL_HEDGE",
                 new ItemStack(Items.ENDER_PEARL, 2),
                 new ItemStack(Items.ENDER_PEARL),
                 new AspectList().add(Aspect.MOTION, 15).add(Aspect.ELDRITCH, 10)
